ec1397acff8dcb649228ea64200a7d32a55006a9
Armonis5 <97055469+Armonis5@users.noreply.github.com>
2023-12-14 21:01:31 +0100

    Feature 5 corrected

--- p3_code/feature_5_generation.py
+++ p3_code/feature_5_generation.py
@@ -3,90 +3,61 @@ import pandas as pd
 import numpy as np
 from pathlib import Path
 
-PATH = Path.cwd()
-PATH = PATH.parent
-DATA_PATH = PATH / "data/wikispeedia_paths-and-graph/"
-PATHS_FINISHED = (DATA_PATH / "paths_finished.tsv").resolve()
-PATHS_UNFINISHED = (DATA_PATH / "paths_unfinished.tsv").resolve()
-shortest_path = (DATA_PATH / "shortest-path-distance-matrix.txt").resolve()
-assert shortest_path.is_file()
-shortest_path = np.loadtxt(shortest_path, dtype=str)
 
-def convert_to_matrix(data):
-    """Replaces each row (string) with the integer values of the string and replaces _ with NaN"""
-    data = np.array([[int(i) if i != "_" else np.nan for i in row] for row in data])
-    return data
-
-shortest_path_matrix = convert_to_matrix(shortest_path)
-articles_path = (DATA_PATH / "articles.tsv").resolve()
-assert articles_path.is_file()
-articles = pd.read_csv(
-    articles_path,
-    sep="\t",
-    header=None,
-    names=["name"],
-    skiprows=11,
-    skip_blank_lines=True,
-)
-articles.name = articles.name.apply(unquote)
-
-
-def compare_paths(user_path, shortest_path_df):
+def compare_paths(user_path, shortest_path_distance_matrix,articles):
     """Looks up for the optimal path and compares it to the user path."""
+    user_path = user_path.split(";")
     start, end = user_path[0], user_path[-1]
-    try:
-        shortest_p = shortest_path_df.loc[start, end]
-        if shortest_p == 0:
-            shortest_p = 1  # FIXME
-        elif np.isnan(shortest_p):
-            return np.nan
-    except KeyError:
-        return np.nan
-    user_len = len(user_path)
-    if np.isnan(shortest_p):
-        return np.nan
-    return user_len / shortest_p
+    opt = 0 
+    start_index = articles[articles["article name"] == start].index[0]
+    end_index = articles[articles["article name"] == end].index[0]
+    if shortest_path_distance_matrix[start_index][end_index] == "_":
+        opt = -1  
+    else:
+        opt = shortest_path_distance_matrix[start_index][end_index]
+    return opt
 
-def average_ratio(dataframe):
-    """Calculates the average ratio for each player"""
-    dataframe = dataframe.groupby("hashedIpAddress").agg(
-        {
-            "ratio": "mean",
-            "path": "count",
-            "durationInSec": "mean",
-            "average_time_on_page": "mean",
-        }
-    )
-    dataframe = dataframe.rename(columns={"path": "count"})
-    dataframe = dataframe.rename(columns={"durationInSec": "mean_duration"})
-    return dataframe
 
-def player_unfinished(dataframe):
-    """Calculates the number of unfinished paths and giving up time average for each player"""
-    dataframe = dataframe.groupby("hashedIpAddress").agg(
-        {"path": "count", "durationInSec": "mean"}
-    )
-    dataframe = dataframe.rename(columns={"path": "count"})
-    dataframe = dataframe.rename(columns={"durationInSec": "mean_duration"})
-    return dataframe
+# def average_ratio(dataframe):
+#     """Calculates the average ratio for each player"""
+#     dataframe = dataframe.groupby("hashedIpAddress").agg(
+#         {
+#             "ratio": "mean",
+#             "path": "count",
+#             "durationInSec": "mean",
+#             "average_time_on_page": "mean",
+#         }
+#     )
+#     dataframe = dataframe.rename(columns={"path": "count"})
+#     dataframe = dataframe.rename(columns={"durationInSec": "mean_duration"})
+#     return dataframe
 
-def average_ratio(dataframe):
-    """Calculates the average ratio for each player"""
-    dataframe = dataframe.groupby("hashedIpAddress").agg(
-        {
-            "ratio": "mean",
-            "path": "count",
-            "durationInSec": "mean",
-            "average_time_on_page": "mean",
-        }
-    )
-    dataframe = dataframe.rename(columns={"path": "count"})
-    dataframe = dataframe.rename(columns={"durationInSec": "mean_duration"})
-    return dataframe
+# def player_unfinished(dataframe):
+#     """Calculates the number of unfinished paths and giving up time average for each player"""
+#     dataframe = dataframe.groupby("hashedIpAddress").agg(
+#         {"path": "count", "durationInSec": "mean"}
+#     )
+#     dataframe = dataframe.rename(columns={"path": "count"})
+#     dataframe = dataframe.rename(columns={"durationInSec": "mean_duration"})
+#     return dataframe
+
+# def average_ratio(dataframe):
+#     """Calculates the average ratio for each player"""
+#     dataframe = dataframe.groupby("hashedIpAddress").agg(
+#         {
+#             "ratio": "mean",
+#             "path": "count",
+#             "durationInSec": "mean",
+#             "average_time_on_page": "mean",
+#         }
+#     )
+#     dataframe = dataframe.rename(columns={"path": "count"})
+#     dataframe = dataframe.rename(columns={"durationInSec": "mean_duration"})
+#     return dataframe
 
 
 
-def add_paths_ratio(df,feature_name="paths_ratio",quiet=False):
+def add_paths_ratio(df,short_matrix,articles,feature_name="paths_ratio",quiet=False):
     """Add a feature to the dataframe that show the ratio between the user paths and the shortest path
     --------------------
     Input:
@@ -101,10 +72,7 @@ def add_paths_ratio(df,feature_name="paths_ratio",quiet=False):
         if not quiet:
             print(f"Warning: the dataframe already contains a column named {feature_name}")
     else:
-        shortest_path_df = pd.DataFrame(
-        shortest_path_matrix, index=articles.name, columns=articles.name
-        )
-        df["ratio"] = df["path"].apply(lambda x: compare_paths(x, shortest_path_df))
+        df["ratio"] = df["path"].apply(lambda x: compare_paths(x, short_matrix,articles))
     return df
 
 def add_average_time_on_page(df,feature_name="average_time_on_page",quiet=False):
