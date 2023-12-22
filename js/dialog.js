51311121ad0aadf752832b0564d60add9ed21960
C-Achard <cyril.achard@epfl.ch>
2023-12-22 19:04:09 +0100

    Update dialog.js

--- js/dialog.js
+++ js/dialog.js
@@ -1,19 +1,19 @@
 const bobMonolog = [
     "Hello there, fellow explorers of this vast digital universe! I'm Bob, your guide and companion in the enthralling world of Wikispeedia.",
     "Imagine, if you will, a place where information stretches as far and wide as stars in a galaxy. That place, my friends, is Wikipedia. And within this boundless expanse of knowledge, lies our adventure.",
-    "Wikispeedia isn't just any ordinary game. Oh no, it's a microcosm of our collective curiosity and intellect.",
-    "In this game, we navigate from one Wikipedia article to another, seemingly unrelated one, using only the hyperlinks as our pathways.",
+    "Wikispeedia isn't just any ordinary game. Oh no, it's a window into humanity's collective curiosity and knowledge.",
+    "In this game, we navigate from one Wikipedia article to a target article, seemingly unrelated, using only the hyperlinks as our pathways.",
     "It's like a digital maze, a condensed version of Wikipedia with over four thousand articles, each a unique playground for the mind.",
     "You see, my adventures in Wikispeedia are more than just a way to pass time. They're a quest – a quest to uncover the hidden pathways of knowledge, to delve deep into the interconnected web of information.",
-    "Each journey I undertake is a discovery, a revelation of the vast diversity that Wikipedia holds.",
+    // "Each journey I undertake is a discovery, a revelation of the vast diversity that Wikipedia holds.",
     "But what really lies at the heart of these adventures? Is it the skill in finding the quickest path, or the strategy born from intuition and understanding of the semantic links between articles?",
     "This, my friends, is what we seek to uncover in our data story.",
-    "As we analyze the gameplay patterns and strategies, not just mine but of many other Wikispeedia enthusiasts, we're uncovering some truly fascinating insights.",
-    "We're going beyond just the surface, exploring how our navigation choices are intertwined with the rich semantic content of Wikipedia.",
-    "We're dissecting paths, extracting features, and revealing the myriad strategies at play.",
-    "Our journey is more than a narrative of digital escapades. It's a deep dive into the human mind's capability to navigate and connect within a sea of information.",
-    "Together, we'll journey through the virtual corridors of Wikipedia, gaining a deeper appreciation of the intricate and often surprising ways in which knowledge is linked.",
-    "So, join me on this journey. Let's unravel the mysteries of Wikispeedia together.",
+    // "As we analyze the gameplay patterns and strategies, not just mine but of many other Wikispeedia enthusiasts, we're uncovering some truly fascinating insights.",
+    // "We're going beyond just the surface, exploring how our navigation choices are intertwined with the rich semantic content of Wikipedia.",
+    // "We're dissecting paths, extracting features, and revealing the myriad strategies at play.",
+    // "Our journey is more than a narrative of digital escapades. It's a deep dive into the human mind's capability to navigate and connect within a sea of information.",
+    // "Together, we'll journey through the virtual corridors of Wikipedia, gaining a deeper appreciation of the intricate and often surprising ways in which knowledge is linked.",
+    // "So, join me on this journey. Let's unravel the mysteries of Wikispeedia together.",
     "Each click, each article, each link we explore is a step towards a greater understanding of how we, as curious beings, interact with and make sense of the vast landscape of information that lies at our fingertips.",
     "Are you ready? Let the adventure begin!"
 ];
@@ -24,21 +24,27 @@ const bobMonolog2 = [
 ];
 
 const adaMonolog = [
-    "Thank you, Bob. Hello, curious minds and brave navigators of this vast digital expanse! I am Ada Lovelace, and it is my pleasure to lead you through the intricate ballet of data analysis.",
-    "As we examine our dataset, we encounter features in the first column that are akin to wild, untamed rivers - heavy-tailed and overflowing with information.",
-    "To channel these waters into a more navigable course, we shall employ a technique from our analytical armory - the logarithmic transformation.",
-    "However, our journey encounters an intriguing obstacle with the 'backtrack' feature, situated in the second row, first column.",
-    "This feature, discrete and steadfast in its few values, eludes the smoothing grace of our logarithmic bridge.",
-    "Our next step in this odyssey is normalization, specifically z-score normalization.",
-    "Imagine this as a way to equalize the heights of mountains and depths of valleys in our data landscape.",
-    "In addressing the voids represented by NaN values, we shall not leave them as unknown territories.",
-    "Instead, we fill these gaps with the mean - a kind of cartographic estimation, ensuring that no piece of our data map remains uncharted.",
-    "Through this process, we do more than just manipulate numbers.",
-    "We engage in a dance with data, leading it gracefully to reveal its hidden patterns and secrets.",
-    "So, join me, as we continue to weave through the tapestry of information, each step a discovery, each transformation a revelation.",
-    "Together, we will uncover the stories hidden within the numbers, exploring the vast landscapes of knowledge that await us.",
-    "Let us embark on this enlightening journey, unraveling the mysteries of data with every analysis we perform.",
-    "The adventure beckons!"
+    "Thank you, Bob. Hello, curious minds and brave navigators of this vast digital expanse! I am Ada Lovelace, and it is my pleasure to lead you through this intricate ballet of data analysis.",
+    "As we examine our dataset, we encounter features in the first column that are strongly heavy-tailed.",
+    "To harness them, we shall employ a technique from our analytical armory - the logarithmic transformation.",
+    "However, we encounter an obstacle with the 'backtrack' feature, situated in the second row, first column.",
+    "This feature, discrete and steadfast in its few values, eludes the smoothing grace of our logarithmic arcanes.",
+    "Our next step is normalization, specifically z-score normalization.",
+    // "Imagine this as a way to equalize the heights of mountains and depths of valleys in our data landscape.",
+    "We shall also address the NaN values, as we do not wish to leave any gaps in our data.",
+    "Instead, we fill these gaps with the mean of each feature - making them meaningful yet unremarkable to our analysis.",
+    // "Through this process, we do more than just manipulate numbers.",
+    // "We engage in a dance with data, leading it gracefully to reveal its hidden patterns and secrets.",
+    // "So, join me, as we continue to weave through the tapestry of information, each step a discovery, each transformation a revelation.",
+    "We are now ready to uncover the stories hidden within the numbers!",
+    // , exploring the vast landscapes of knowledge that await us.",
+    // "Let us embark on this enlightening journey, unraveling the mysteries of data with every analysis we perform.",
+    // "The adventure beckons!"
+    // Yeesha
+    // Last night your mother had a dream...
+    // We know that some futures are not cast, by writer or Maker, but the dream tells that D'ni will grow again someday. New seekers of D'ni will flow in from the desert feeling called to something they do not understand.
+    // But the dream also tells of a desert bird with the power to weave this new D'ni's future. We fear such power - it changes people.
+    // Yeesha, our desert bird, your search seems to take you further and further from us. I hope what you find will bring you closer. 
 ];
 
 const bobMonolog3 = [
